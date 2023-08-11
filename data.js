61826240a081dd5b56da1a0e8370ac452544d741
rihabcherni <rihabcherni000@gmail.com>
2023-08-11 14:11:28 +0100

    update

--- data.js
+++ data.js
@@ -1,1 +1,31 @@
-DataView
+const quotes_array = [
+    "Push yourself, because no one else is going to do it for you.",
+    "Failure is the condiment that gives success its flavor.",
+    "Wake up with determination. Go to bed with satisfaction.",
+    "It's going to be hard, but hard does not mean impossible.",
+    "Learning never exhausts the mind.",
+    "The only way to do great work is to love what you do.",
+    "IEEE Computer Society (CS): Focusing on computer science and technology.",
+    "IEEE Communications Society (ComSoc): Covering communication systems and technologies.",
+    "IEEE Power & Energy Society (PES): Concentrating on power generation, distribution, and utilization.",
+    "IEEE Robotics and Automation Society (RAS): Dealing with robotics and automation technologies.",
+    "IEEE Signal Processing Society (SPS): Focusing on signal processing techniques and applications.",
+    "IEEE Aerospace and Electronic Systems Society (AESS): Covering aerospace systems and electronic technologies.",
+    "IEEE Circuits and Systems Society (CAS): Concentrating on circuits and systems theory and applications.",
+    "IEEE Microwave Theory and Techniques Society (MTT-S): Dealing with microwave and radio frequency technologies.",
+    "IEEE Consumer Electronics Society (CES): Focusing on consumer electronics and technologies.",
+    "IEEE Industrial Electronics Society (IES): Covering industrial electronics and applications.",
+    "IEEE Instrumentation and Measurement Society (IMS): Dealing with instrumentation and measurement technologies.",
+    "IEEE Computational Intelligence Society (CIS): Concentrating on computational intelligence methods.",
+    "IEEE Photonics Society: Focusing on photonics and optical technologies.",
+    "IEEE Vehicular Technology Society (VTS): Covering vehicular technologies and applications.",
+    "IEEE Information Theory Society (ITS): Dealing with information theory and coding techniques.",
+    "IEEE Nanotechnology Council (NTC): Concentrating on nanotechnology and related fields.",
+    "IEEE Systems, Man, and Cybernetics Society (SMC): Focusing on systems, man, and cybernetics.",
+    "IEEE Education Society: Covering educational methods and technologies.",
+    "IEEE Biometrics Council: Dealing with biometrics and related areas.",
+    "IEEE Technology and Engineering Management Society (TEM): Concentrating on technology and engineering management."
+];
+  
+
+  
--- game.js
+++ game.js
@@ -1,15 +1,50 @@
+function loadfunc() {    
+  var load=document.getElementById("load");
+  load.remove();
+  // const game = document.getElementById("container-game");
+  // game.style.display = "block";
+  const formulaire = document.getElementById("formulaire");
+  formulaire.style.display = "block";
+};
+function loading() {
+  const alphabetCs = 'CscS!CscS!CscS!CscS!CscS!CscS!CscS!CscS!CscS!CscS!CscS!CscS!CscS!CscS!';
+  const alphabetIeee = 'IEEEENSIT!IEEEENSIT!IEEEENSIT!IEEEENSIT!IEEEENSIT!IEEEENSITIEEEENSIT';
+  const lettreCs="letterCS";
+  const lettreIeee="lettreIeee";
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function createLetter(letter, classLettre) {
+    const element = document.createElement('div');
+    if(classLettre==='letterCS'){
+      element.className = "letterCS";
+    }else if(classLettre==='lettreIeee'){
+      element.className = "lettreIeee";
+    }
+    element.style.left = getRandomInt(0, window.innerWidth) + 'px';
+    element.style.top = getRandomInt(0, window.innerHeight) + 'px';
+    element.style.transform = `rotate(${getRandomInt(-180, 180)}deg)`;
+    element.textContent = letter;
+    document.getElementById("load").appendChild(element);
+  }
+  for (const letter of alphabetCs) {
+    createLetter(letter,lettreCs );
+  }
+  for (const letter of alphabetIeee) {
+    createLetter(letter,lettreIeee);
+  }
+  setTimeout(loadfunc, 7000)
+}
+function Submitlogin() {    
+  var formulaire=document.getElementById("formulaire");
+  formulaire.remove();
+  const game = document.getElementById("container-game");
+  game.style.display = "block";
+};
+
 // define the time limit
 let TIME_LIMIT = 60;
-
-// define quotes to be used
-let quotes_array = [
-  "Push yourself, because no one else is going to do it for you.",
-  "Failure is the condiment that gives success its flavor.",
-  "Wake up with determination. Go to bed with satisfaction.",
-  "It's going to be hard, but hard does not mean impossible.",
-  "Learning never exhausts the mind.",
-  "The only way to do great work is to love what you do."
-];
 
 // selecting required elements
 let timer_text = document.querySelector(".curr_time");
@@ -24,6 +59,7 @@ let cpm_group = document.querySelector(".cpm");
 let wpm_group = document.querySelector(".wpm");
 let error_group = document.querySelector(".errors");
 let accuracy_group = document.querySelector(".accuracy");
+let nbWords = document.querySelector(".curr_allWord");
 
 let timeLeft = TIME_LIMIT;
 let timeElapsed = 0;
@@ -32,26 +68,38 @@ let errors = 0;
 let accuracy = 0;
 let characterTyped = 0;
 let current_quote = "";
-let quoteNo = 0;
 let timer = null;
+nbWords.innerHTML = quotes_array.length;
 
+input_area.onpaste = function () {
+  return false;
+}
+let original_quotes_array = [...quotes_array];
+  
 function updateQuote() {
+  if (quotes_array.length === 0) {
+    // All phrases have been used, reset the array
+    quotes_array = original_quotes_array.slice();
+  }
+
   quote_text.textContent = null;
-  current_quote = quotes_array[quoteNo];
 
-  // separate each character and make an element 
-  // out of each of them to individually style them
+  const randomIndex = Math.floor(Math.random() * quotes_array.length);
+  current_quote = quotes_array[randomIndex];
+
+  // Remove the selected phrase from the array
+  quotes_array.splice(randomIndex, 1);
+  console.log(quotes_array)
+  // Update the main quotes_array only when quotes_array is empty
+  if (quotes_array.length === 0) {
+    quotes_array = [];
+  }
+
   current_quote.split('').forEach(char => {
-    const charSpan = document.createElement('span')
-    charSpan.innerText = char
-    quote_text.appendChild(charSpan)
-  })
-
-  // roll over to the first quote
-  if (quoteNo < quotes_array.length - 1)
-    quoteNo++;
-  else
-    quoteNo = 0;
+    const charSpan = document.createElement('span');
+    charSpan.innerText = char;
+    quote_text.appendChild(charSpan);
+  });
 }
 
 function processCurrentText() {
@@ -152,18 +200,13 @@ function finishGame() {
   cpm_group.style.display = "block";
   wpm_group.style.display = "block";
 }
-
-
 function startGame() {
-
   resetValues();
   updateQuote();
-
   // clear old and start a new timer
   clearInterval(timer);
   timer = setInterval(updateTimer, 1000);
 }
-
 function resetValues() {
   timeLeft = TIME_LIMIT;
   timeElapsed = 0;
@@ -171,7 +214,6 @@ function resetValues() {
   total_errors = 0;
   accuracy = 0;
   characterTyped = 0;
-  quoteNo = 0;
   input_area.disabled = false;
 
   input_area.value = "";
@@ -182,4 +224,5 @@ function resetValues() {
   restart_btn.style.display = "none";
   cpm_group.style.display = "none";
   wpm_group.style.display = "none";
+  nbWords.innerHTML = quotes_array.length;
 }
