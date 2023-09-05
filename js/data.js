d7a8c8d3317de163676b659ff3765fbb6af95a70
Hadj Sassi Mahdi <74431887+HadjSassi@users.noreply.github.com>
2023-09-05 21:53:29 +0100

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,11 +1,6 @@
 let quotes_easy = [
     /*  other club */
     "IEEE student Branch",
-    
-];
-
-let quotes_medium = [
-    /*     ensit info     */
     "ENSIT: Ecole nationale superieure d'ingenieurs de Tunis",
     "Civil Engineering",
     "Industrial Engineering",
@@ -13,7 +8,6 @@ let quotes_medium = [
     "Electrical Engineering",
     "Mathematics and Modeling Engineering",
     "Mechanical Engineering",
-    /*     ieee info     */
     "IEEE: Institute of Electrical and Electronics Engineers",
     "Computer Society CS",
     "Robotics and Automation Society RAS",
@@ -21,31 +15,60 @@ let quotes_medium = [
     "Aerospace and Electronic Systems Society AESS",
     "Power & Energy Society PES",
     "Women in Engineering WIE",
-
+    "IEEE members innovate for a better future.",
+    "CS Chapter fosters tech skills in students.",
+    "Xtreme coding challenges improve skills.",
+    "IEEE connects tech minds worldwide.",
+    "CS Chapter hosts coding competitions.",
+    "Xtreme programming sharpens developer skills.",
+    "IEEE advances technology through research.",
+    "CS Chapter mentors future tech leaders.",
+    "Xtreme coding tests your programming agility.",
+    "IEEE inspires engineers to excel.",
 ];
 
-let quotes_hard = [
+let quotes_medium = [
+    "IEEE, a global organization, brings together professionals to innovate, research, and create groundbreaking technologies that shape our world.",
+    "CS Chapter, a community of passionate computer scientists, provides resources, workshops, and events to empower students in mastering modern coding techniques.",
+    "Xtreme programming, an agile approach to software development, encourages collaboration, quick iterations, and constant feedback for achieving high-quality code and solutions.",
+    "IEEE, with its vast network, offers opportunities to connect, collaborate, and learn from experts, making it a valuable resource for engineers in diverse fields.",
+    "CS Chapter, dedicated to promoting computer science education, offers hands-on experiences and skill-building activities to prepare students for the tech industry.",
+    "Xtreme programming challenges developers to embrace change, focus on customer needs, and deliver exceptional software through continuous testing and integration.",
+    "IEEE, at the forefront of technology trends, empowers professionals to tackle complex challenges and drive innovation for the betterment of society.",
+    "CS Chapter cultivates an environment where students can explore emerging technologies, connect with peers, and develop the skills needed for a successful tech career.",
+    "Xtreme programming's principles of simplicity, communication, and feedback help teams create software that adapts to evolving requirements and delivers value.",
+    "IEEE inspires engineers to make a positive impact on the world, pushing the boundaries of what's possible through collaboration, research, and innovation.",
     "Push yourself, because no one else is going to do it for you",
     "Failure is the condiment that gives success its flavor",
     "Wake up with determination. Go to bed with satisfaction",
     "It's going to be hard, but hard does not mean impossible",
     "Learning never exhausts the mind",
     "The only way to do great work is to love what you do",
-
-    /* new academic journey  */
     "A new chapter of learning and growth begins",
     "Dream, learn, achieve: your university voyage begins now",
     "Discover, grow, excel: seize the opportunities of university life.",
     "Aspire, achieve, repeat: write your chapter of success at university",
     "Innovate, collaborate, elevate: forge your path in university",
     "Empower your journey: launching into excellence at university",
-    /* or  */
     "Computer Society CS: Focusing on computer science and technology",
     "Robotics and Automation Society RAS: Dealing with robotics and automation technologies",
     "Industry Applications Society IAS: Centered on industrial and technological applications",
     "Aerospace and Electronic Systems Society AESS: Covering aerospace systems and electronic technologies",
     "Power & Energy Society PES: Concentrating on power generation, distribution, and utilization",
     "Women in Engineering WIE: Empowering women, promoting diversity, and fostering innovation in engineering",
+];
+
+let quotes_hard = [
+    "IEEE, an esteemed global organization, unites professionals from diverse fields, fostering innovation, research, and the creation of transformative technologies that redefine our future.",
+    "CS Chapter, a vibrant community of dedicated computer scientists, empowers students with an extensive array of resources, workshops, and events to master advanced coding techniques.",
+    "Xtreme programming, an agile and disciplined approach to software development, champions collaboration, rapid iterations, and continuous feedback to achieve exceptional code quality and solutions.",
+    "IEEE, with its extensive network spanning the globe, offers unparalleled opportunities to connect, collaborate, and gain insights from leading experts, making it an indispensable resource for engineers across industries.",
+    "CS Chapter, committed to advancing computer science education, equips students with hands-on experiences and skill-building activities that prepare them for thriving careers in the ever-evolving tech sector.",
+    "Xtreme programming propels developers to embrace change, prioritize customer needs, and deliver outstanding software through relentless focus on continuous testing, integration, and relentless improvement.",
+    "IEEE stands as a trailblazer in technology trends, empowering professionals to confront intricate challenges head-on and steer the course of innovation for the betterment of society at large.",
+    "CS Chapter fosters an ecosystem where students explore cutting-edge technologies, connect with like-minded peers, and cultivate the skills required for a thriving career in the tech industry.",
+    "Xtreme programming's core tenets of simplicity, open communication, and embracing feedback guide teams in crafting adaptable software solutions that seamlessly evolve with shifting requirements and deliver enduring value.",
+    "IEEE inspires engineers to embark on a journey that transcends boundaries, encouraging them to shape a brighter future through unparalleled collaboration, groundbreaking research, and relentless innovation.",
 ];
 
 
